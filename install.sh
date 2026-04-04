52f71631947ff2b20cc9f67b977d66ad1cdbe225
jmxyyy <jmxyyy03@gmail.com>
2026-04-04 19:32:01 +0800

    init

---
+++ install.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://raw.githubusercontent.com/uton88/dan-binary-releases/main/install.sh | bash -s -- \
+  --cpa-base-url 'http://localhost:8317/' \
+  --cpa-token '7403' \
+  --mail-api-url 'https://gpt-mail.icoa.pp.ua/' \
+  --mail-api-key 'linuxdo' \
+  --threads 50
